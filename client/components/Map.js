e65e842096bed26aad78b3db4bd872d8ad10d3c3
sjsamphex <sjsamphex@gmail.com>
2021-03-04 10:15:25 -0800

    remove comments and broken code

--- client/components/Map.js
+++ client/components/Map.js
@@ -13,10 +13,6 @@ function Map() {
       <GoogleMap
         defaultZoom={10}
         defaultCenter={{ lat: 40.7127281, lng: -74.0060152 }}
-        fullscreenControl={false}
-        // zoomControl: false,
-        // mapTypeControl: false,
-        // streetViewControl: false
       >
         <Marker position={{ lat: 40.7127281, lng: -74.0060152 }} />
       </GoogleMap>
