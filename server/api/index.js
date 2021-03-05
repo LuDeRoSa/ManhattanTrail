8d9c50eb9dccb5fdb6a4c31bc98413b68bee2b63
Devy Agarwal <da5ce@virginia.edu>
2021-03-05 14:33:32 -0500

    path route

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,11 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/path', require('./path'))
+// router.use('/quiz', require('./quiz'))
+// router.use('/responses', require('./responses'))
+// router.use('/restaurant', require('./restaurant'))
+// router.use('/scores', require('./scores'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
---
+++ server/api/path.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const {
+  models: {Restaurant, Path, Quiz},
+} = require("../db");
+
+
+module.exports = router;
+
+router.get('/:id/restaurants', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const restaurant = await Restaurant.findbyPk(id);
+    const quiz = await Quiz.findbyPk(id);
+
+    const dataReturn = {
+      restaurant,
+      quiz,
+    };
+
+    // res.send("testing this path to see if it exists!")
+    res.send(dataReturn);
+
+
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+
+// router.get('/:id/restaurants', async (req, res, next) => {
+//   try {
+//     const id = req.params.id;
+//     const restaurant = await Restaurant.findbyPk(id);
+//     const quiz = await Quiz.findbyPk(id);
+
+
+//   } catch (err) {
+//     next(err);
+//   }
+// });
