812fa36bd1cd932e703c5e66e68636a11f9fb426
Rommel Demano <youremail>
2021-03-06 13:10:37 -0500

    new map

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,55 +1,106 @@
 import React, { useState } from "react";
-import {
-  GoogleMap,
-  withScriptjs,
-  withGoogleMap,
-  Marker,
-  InfoWindow,
-} from "react-google-maps";
+// import {
+//   GoogleMap,
+//   withScriptjs,
+//   withGoogleMap,
+//   Marker,
+//   InfoWindow,
+// } from "react-google-maps";
 
+import GoogleMapReact from "google-map-react";
+import { connect } from "react-redux";
+import { setRest } from "../store/restaurants";
 //dummy data
 const points = [
   { id: 1, title: "The Smith", lat: 40.741895, lng: -73.989308 },
   { id: 2, title: "The Hillstone", lat: 40.7580445, lng: -73.9699967 },
   { id: 3, title: "Boqueria", lat: 40.77152, lng: -73.9561132 },
 ];
+// function Map() {
+//   const [marker, setMarker] = useState(null);
+//   return (
+//     <div>
+//       <GoogleMap
+//         defaultZoom={10}
+//         defaultCenter={{ lat: 40.7127281, lng: -74.0060152 }}
+//       >
+//         {/* //this is the initial marker, attached an onClick that displays the rest
+//         of the Marker(path) */}
+//         <Marker
+//           position={{ lat: 40.7127281, lng: -74.0060152 }}
+//           onClick={() => {
+//             setMarker(
+//               points.map((point) => (
+//                 <Marker
+//                   key={point.id}
+//                   position={{ lat: point.lat, lng: point.lng }}
+//                 />
+//               ))
+//             );
+//           }}
+//         />
+//         {/* conditionally renders the marker once its clicked  */}
+//         {marker &&
+//           points.map((point) => (
+//             <Marker
+//               key={point.id}
+//               position={{ lat: point.lat, lng: point.lng }}
+//             />
+//           ))}
+//       </GoogleMap>
+//     </div>
+//   );
+// }
 
-function Map() {
-  const [marker, setMarker] = useState(null);
-  return (
-    <div>
-      <GoogleMap
-        defaultZoom={10}
-        defaultCenter={{ lat: 40.7127281, lng: -74.0060152 }}
-      >
-        {/* //this is the initial marker, attached an onClick that displays the rest
-        of the Marker(path) */}
-        <Marker
-          position={{ lat: 40.7127281, lng: -74.0060152 }}
-          onClick={() => {
-            setMarker(
-              points.map((point) => (
-                <Marker
-                  key={point.id}
-                  position={{ lat: point.lat, lng: point.lng }}
-                />
-              ))
-            );
+class _Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      marker: [],
+      restaurants: [],
+    };
+    this.setMarker = this.setMarker.bind(this);
+  }
+
+  componentDidMount() {
+    console.log(this.props.setRest());
+  }
+
+  setMarker(arr) {
+    this.setState({
+      marker: arr,
+    });
+  }
+
+  render() {
+    const { setMarker } = this;
+    return (
+      <div style={{ height: "100vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: "AIzaSyCnNLEaNM_3zfMo0yHe - nINMSUPPfyJwUI",
           }}
-        />
-        {/* conditionally renders the marker once its clicked  */}
-        {marker &&
-          points.map((point) => (
-            <Marker
-              key={point.id}
-              position={{ lat: point.lat, lng: point.lng }}
-            />
-          ))}
-      </GoogleMap>
-    </div>
-  );
+          defaultZoom={10}
+          defaultCenter={{ lat: 40.7127281, lng: -74.0060152 }}
+        ></GoogleMapReact>
+        <div>
+          <button onClick={() => this.setMarker()}>Next</button>
+        </div>
+      </div>
+    );
+  }
 }
 
-const MapWrapped = withScriptjs(withGoogleMap(Map));
+//changed the map package, converted it into a class. must fix the route to grab the right data.
+//working on that next, pushing this for now so Samir and i can work on the same repo.
 
-export default MapWrapped;
+const mapState = (state) => {
+  return state;
+};
+const mapDispatch = (dispatch) => {
+  return {
+    setRest: () => dispatch(setRest()),
+  };
+};
+
+export default connect(mapState, mapDispatch)(_Map);
