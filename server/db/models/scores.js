4ba5fc94bb9f01ea7c0ea5bed90eddc31ecbd3a4
Samir Jain <sjsamphex@gmail.com>
2021-03-16 11:26:22 -0700

    remove dead columns and comments from Scores model

--- server/db/models/scores.js
+++ server/db/models/scores.js
@@ -2,16 +2,6 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Scores = db.define('scores', {
-  // game_id: {
-  //   type: Sequelize.INTEGER,
-  //   primaryKey: true,
-  // },
-  //commented out because sequelize relationships will assign a gameId anyway
-  user_id: {
-    type: Sequelize.INTEGER,
-  },
-  //commented out because Game already has information about userId
-  //we commented this back so we can update user score from the quiz
   total_score: {
     type: Sequelize.INTEGER,
     defaultValue: 0,
