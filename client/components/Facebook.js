e4bbec9a292e7ae742685dea7751cd06f04a6093
Rdgh1583 <rommeldemano@gmail.com>
2021-03-25 13:40:35 -0400

    hooked up favorites on profile page

---
+++ client/components/Facebook.js
@@ -0,0 +1,29 @@
+// import React, { Component } from 'react';
+// import FacebookLogin from 'react-facebook-login';
+
+// class Facebook extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+
+//   responseFacebook(response) {
+//     console.log(response);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <FacebookLogin
+//           appId='459306878548339'
+//           autoLoad={true}
+//           fields='name,email,picture'
+//           onClick={this.componentClicked}
+//           callback={this.responseFacebook}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default Facebook;
--- client/store/favorites.js
+++ client/store/favorites.js
@@ -6,11 +6,13 @@ const getToken = () => window.localStorage.getItem('token');
  */
 
 const ADD_FAV = 'ADD_FAV';
+const GET_FAV = 'GET_FAV';
 /**
  * ACTION CREATORS
  */
 
 const _addFav = (rest) => ({ type: ADD_FAV, rest });
+const _getFav = (favorites) => ({ type: GET_FAV, favorites });
 
 /**
  * THUNK CREATORS
@@ -18,7 +20,7 @@ const _addFav = (rest) => ({ type: ADD_FAV, rest });
 
 export const addFav = (restId) => async (dispatch) => {
   const token = getToken();
-  console.log('store', restId);
+  // console.log('store', restId);
   const rest = (
     await axios.post(
       `/api/favorite/addFave`,
@@ -33,16 +35,32 @@ export const addFav = (restId) => async (dispatch) => {
   return dispatch(_addFav(rest));
 };
 
+export const getFav = (userId) => async (dispatch) => {
+  const token = getToken();
+
+  // console.log('store', userId);
+  const favorites = (
+    await axios.get(`/api/favorite/${userId}`, {
+      headers: {
+        authorization: token,
+      },
+    })
+  ).data;
+  return dispatch(_getFav(favorites));
+  // console.log('store', rest);
+};
 /**
  * REDUCER
  */
 const initState = {
-  rests: [],
+  favorites: [],
 };
 export default function (state = initState, action) {
   switch (action.type) {
-    case ADD_FAVE:
+    case ADD_FAV:
       return { ...state, rests: action.rests };
+    case GET_FAV:
+      return { ...state, favorites: action.favorites };
     default:
       return state;
   }
