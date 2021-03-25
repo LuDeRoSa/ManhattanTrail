2877a1672f5523193889cb4d64ee1ae67ff40b68
supremequeen189 <luisa@luisazhou.com>
2021-03-24 21:47:39 -0400

    save

--- client/components/PhaserGameScore.js
+++ client/components/PhaserGameScore.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-
+import { updateMiniGameScore } from "../store/game.js"
 class PhaserGameScore extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    debugger;
-    return <div>hi you have this many points! {this.props.points}</div>;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+        }
+        componentDidUpdate(prevProps, prevState, snapshot) {
+            this.props.updatePhaserGameScore(this.props.game.score);
+        }
+    render() {
+        return <div>hi you have this many points! {this.props.game.score}</div>;
+        }
 }
-
-export default connect((state) => state)(PhaserGameScore);
+function mapDispatchToProps(dispatch) {
+    return {
+        updatePhaserGameScore: (points) => dispatch(updateMiniGameScore(points))
+    }
+}
+export default connect((state) => state, mapDispatchToProps)(PhaserGameScore);
