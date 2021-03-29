d280cf56b37a2cf6ebadba69dd992de92585ce99
Samir Jain <sjsamphex@gmail.com>
2021-03-29 07:54:58 -0700

    ev prevent default on keypress down so page doesnt move around

--- client/components/Snake.js
+++ client/components/Snake.js
@@ -6,16 +6,11 @@ export default (props) => {
       {props.snakeDots.map((dot, i) => {
         const style = {
           left: `${dot[0]}%`,
-          top: `${dot[1]}%`
-        }
+          top: `${dot[1]}%`,
+        };
 
-        return (
-          <div className="snake-dot" key={i} style={style}></div>
-        )
+        return <div className="snake-dot" key={i} style={style}></div>;
       })}
     </div>
-
-  )
-
-
-}
+  );
+};
