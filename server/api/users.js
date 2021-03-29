b572df13bd52db5a0ded4e4280ce4ae9f91302b4
Samir Jain <sjsamphex@gmail.com>
2021-03-28 17:44:36 -0700

    delete commented user code and comment out users all route

--- server/api/users.js
+++ server/api/users.js
@@ -4,19 +4,20 @@ const {
 } = require('../db');
 module.exports = router;
 
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'username'],
-    });
-    res.json(users);
-  } catch (err) {
-    next(err);
-  }
-});
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const users = await User.findAll({
+//       // explicitly select only the id and username fields - even though
+//       // users' passwords are encrypted, it won't help if we just
+//       // send everything to anyone who asks!
+//       attributes: ['id', 'username'],
+//     });
+//     res.json(users);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
 router.get('/:id', async (req, res, next) => {
   try {
     const userId = req.params.id;
@@ -30,33 +31,3 @@ router.get('/:id', async (req, res, next) => {
     next(err);
   }
 });
-
-// router.put('/:user_id/:game_id/:question_id'),
-//   async (req, res, next) => {
-//     try {
-//       const userId = req.params.user_id;
-//       const gameId = req.params.game_id;
-//       const questionId = req.params.question_id;
-
-//       console.log('THE PUT LOG!!!!!!!!!!!!!', userId, gameId, questionId);
-//       // const totalScore = await Scores.find({
-//       //   where: {
-//       //     game_id: gameId,
-//       //     user_id: userId,
-//       //     question_id: questionId
-//       //   }
-//       // })
-
-//       // const newScore = totalScore+=1;
-
-//       // totalScore.update({
-//       //   totalScore: newScore
-//       // });
-
-//       // await totalScore.save();
-
-//       // res.send(totalScore);
-//     } catch (ex) {
-//       next(ex);
-//     }
-//   };
