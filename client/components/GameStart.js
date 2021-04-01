cfacba3c7513b58d8ed090a290874e598a97115f
Samir Jain <sjsamphex@gmail.com>
2021-03-31 17:02:03 -0700

    phaser games removal

---
+++ client/components/GameStart.js
@@ -0,0 +1,54 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+//
+import SnakeGame from './SnakeGame';
+import FlappyCake from './FlappyCake';
+
+class GameStart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gameStarted: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ gameStarted: true });
+  }
+
+  render() {
+    switch (this.props.game_type) {
+      case 'snake':
+        return (
+          <div>
+            <h2>Snake Game:</h2>
+            <p>Snake game default text to be edited</p>
+            {this.state.gameStarted ? (
+              <SnakeGame />
+            ) : (
+              <button onClick={this.handleClick}>Start Game</button>
+            )}
+          </div>
+        );
+      case 'flappy':
+        return (
+          <div>
+            <h2>FlappyCake Game:</h2>
+            <p>Press spacebar, up, or click to raise Penguin to catch cakes</p>
+            {this.state.gameStarted ? (
+              <FlappyCake />
+            ) : (
+              <button onClick={this.handleClick}>Start Game</button>
+            )}
+          </div>
+        );
+      default:
+        return <></>;
+    }
+  }
+}
+
+export default GameStart;
