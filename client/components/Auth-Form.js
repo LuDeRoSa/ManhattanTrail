7829f8d04af2fb512613d6b68bc58d695a474cf9
Rdgh1583 <rommeldemano@gmail.com>
2021-04-02 13:06:50 -0400

    deploying to heroku test

--- client/components/Auth-Form.js
+++ client/components/Auth-Form.js
@@ -49,7 +49,7 @@ const AuthForm = (props) => {
 };
 
 /**
- * CONTAINER
+ * CONTAIN
  *   Note that we have two different sets of 'mapStateToProps' functions -
  *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
  *   function, and share the same Component. This is a good example of how we
