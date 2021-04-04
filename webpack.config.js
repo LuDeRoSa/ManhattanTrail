1289de4913fa031653cd998d2c0b5a8eba8fc4a1
Samir Jain <sjsamphex@gmail.com>
2021-04-04 08:28:44 -0700

    remove comments in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,13 @@ module.exports = {
     filename: './public/bundle.js',
   },
   devtool: 'source-map',
-  /*
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: ['style-loader', 'css-loader'],
         exclude: /node_modules/,
-       // loader: 'babel-loader',
-    /*    options: {
+        loader: 'babel-loader',
+        options: {
           presets: [
             '@babel/preset-react',
             { plugins: ['@babel/plugin-proposal-class-properties'] },
@@ -21,28 +19,9 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
     ],
-*/
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-         loader: 'babel-loader',
-      options: {
-      presets: [
-        '@babel/preset-react',
-        { plugins: ['@babel/plugin-proposal-class-properties'] },
-      ],
-    },
   },
-  {
-    test: /\.css$/,
-    use: ['style-loader', 'css-loader']
-  }
-]
-}
 };
