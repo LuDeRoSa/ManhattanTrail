b91928c1016dc24993bb5a06778b2f7e804adf1c
Samir Jain <sjsamphex@gmail.com>
2021-04-06 08:52:08 -0700

    Card

--- client/components/SortFruitsGame/SortFruitsDropzone.js
+++ client/components/SortFruitsGame/SortFruitsDropzone.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Droppable, Draggable } from 'react-beautiful-dnd';
 import '../Style/SortFruits.css';
+
 import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
 const SortFruitsDropzone = ({ isDropDisabled, foods, id }) => (
   <Paper variant="outlined">
@@ -36,18 +43,20 @@ const Food = ({ name, index }) => (
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          <figure
-            style={{ backgroundColor: 'transparent' }}
-            className="avatar tile-icon"
-          >
-            <img
-              height={'64'}
-              width={'90'}
-              src={`./img/${name.toLowerCase().replace(' ', '-')}.jpeg`}
-              alt={name}
-            />
-          </figure>
-          <div className="tile-content">{name}</div>
+          <Card>
+            <figure
+              style={{ backgroundColor: 'transparent' }}
+              className="avatar tile-icon"
+            >
+              <img
+                height={'64'}
+                width={'90'}
+                src={`./img/${name.toLowerCase().replace(' ', '-')}.jpeg`}
+                alt={name}
+              />
+            </figure>
+            <div className="tile-content">{name}</div>
+          </Card>
         </div>
       );
     }}
