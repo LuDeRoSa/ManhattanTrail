1e0e22695d2c29b3e01253407db9f37ad57c9da9
Devy Agarwal <da5ce@virginia.edu>
2021-04-06 13:11:45 -0400

    game over page done

--- client/components/GameOver.js
+++ client/components/GameOver.js
@@ -7,6 +7,7 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
 import PublicIcon from '@material-ui/icons/Public';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import GlobalScore from './GlobalScore';
 import './Style/GameOver.css';
 
 const styles = {
@@ -24,21 +25,32 @@ class GameOver extends React.Component {
     const { game } = this.props;
     return (
       <Container style={styles.container}>
-        <h1 className="game-over-heading">GAME OVER</h1>
-        <div className="score-box">Total Amount of Food Items Donated: {game.total_score}</div>
+        <div className="game-over-heading">GAME OVER</div>
+        <h2>Total Amount of Food Items Donated: {game.total_score}</h2>
         <div className="empty-box"></div>
         <h3>Help combat food waste!</h3>
 
         <div className="food-shelter">
 
           <img src="./img/food-donation.jpeg"/>
-          <p> US restaurants generate an estimated 22 to 33 billion pounds of food waste each year. Drivers of food waste at restaurants include oversized portions, inflexibility of chain store management and extensive menu choices. 41 According to the Cornell University Food and Brand Lab, on average, diners leave 17 percent of their meals uneaten and 55 percent of edible leftovers are left at the restaurant. When the pandemic first happened there was a lot of extra waste,” Gunders tells Food Tank. She explains that the sudden closures of businesses created bottlenecks in supply chains. As a result, food could not reach people fast enough. Gunders explains that the pandemic has forced many restaurants to shift away from buffet models, a major contributor of food waste. She also says that restaurants, uncertain of their business, are offering smaller menus and, as a result, are keeping smaller inventories. List of food pantries in New York City that you can help contribute towards.</p>
-          <p>New York Common Pantry</p>
-          <a href="https://nycommonpantry.org/donate-to-new-york-common-pantry/">Donate Here!</a>
+          <br />
+          <p> US restaurants generate an estimated 22 to 33 billion pounds of food waste each year. Drivers of food waste at restaurants include oversized portions, inflexibility of chain store management and extensive menu choices. According to the Cornell University Food and Brand Lab, on average, diners leave 17 percent of their meals uneaten and 55 percent of edible leftovers are left at the restaurant. When the pandemic first happened there was a lot of extra waste because the sudden closures of businesses created bottlenecks in supply chains. As a result, food could not reach people fast enough. The pandemic has forced many restaurants to innovate, cut back, and better utilize their resources. In doing so, they have shifted away from buffet models, a major contributor of food waste. They are also offering smaller menus and, as a result, are keeping smaller inventories. These new shifts will hopefully help ensure less food waste going forward. One of the best ways to help reduce food waste is to provide extra food to those in need, via food pantries and food shelters. Our game seeks to bring awareness to this issue and show how restaurants and food shelters can work together to reduce food waste. </p>
+
+          <p><b>Food pantries in New York City that you can help support! </b></p>
+
+          <p>
+            New York Common Pantry <a href="https://nycommonpantry.org/donate-to-new-york-common-pantry/">Donate Here!</a>
+          <br/>
+          Food Bank for New York City <a href="https://secure3.convio.net/fbnyc/site/Donation2;jsessionid=00000000.app30050b?df_id=8905&mfc_pref=T&8905.donation=form1&commas=yes&s_src=topmenu&_ga=2.206441175.55822074.1617727504-2002165381.1617727504&NONCE_TOKEN=3D2045D677ADD84139E791DA22CB4DC2">Donate Here!</a>
+          <br/>
+          City Harvest <a href="https://secure.cityharvest.org/site/Donation2?2621.donation=form1&df_id=2621&mfc_pref=T">Donate Here!</a>
+
+          </p>
+
         </div>
-        <p>Path: {game.pathId}</p>
+        {/* <p>Path: {game.pathId}</p>
         <p>Stage: {game.gameStage}</p>
-        <p>Status: {game.status}</p>
+        <p>Status: {game.status}</p> */}
 
         <ButtonGroup>
           <Button
