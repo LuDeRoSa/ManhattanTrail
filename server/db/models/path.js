551edec00950c53d56fcd844fd8a641691b626ff
Samir Jain <sjsamphex@gmail.com>
2021-04-08 12:40:50 -0700

    remove path gametype randomizer. gametypes should be curated in seeding now

--- server/db/models/path.js
+++ server/db/models/path.js
@@ -10,13 +10,13 @@ const Path = db.define('path', {
   },
   game_type: {
     type: Sequelize.ENUM(...GAME_OPTIONS),
-  }
+  },
 });
-Path.addHook('afterCreate', async (path) => {
-  const gameIndex = (path.id - 1) % GAME_OPTIONS.length;
-  if (!path.game_type) {
-    path.game_type = GAME_OPTIONS[gameIndex];
-    await path.save();
-  }
-});
+// Path.addHook('afterCreate', async (path) => {
+//   const gameIndex = (path.id - 1) % GAME_OPTIONS.length;
+//   if (!path.game_type) {
+//     path.game_type = GAME_OPTIONS[gameIndex];
+//     await path.save();
+//   }
+// });
 module.exports = Path;
