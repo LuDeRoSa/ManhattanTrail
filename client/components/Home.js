dd12829107c85e7109d0642311373d49ed6ecedb
Samir Jain <sjsamphex@gmail.com>
2021-04-09 14:40:30 -0700

    imports

--- client/components/Home.js
+++ client/components/Home.js
@@ -9,6 +9,12 @@ import GameStart from './GameStart';
 
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export const Home = (props) => {
