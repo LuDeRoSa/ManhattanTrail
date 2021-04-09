6d37e9b39fdc5e6d2f58f1ea27585ab6e1950ea8
Samir Jain <sjsamphex@gmail.com>
2021-04-09 14:24:29 -0700

    next display

--- client/components/Home.js
+++ client/components/Home.js
@@ -30,7 +30,7 @@ export const Home = (props) => {
           onClick={props.nextStage}
         >
           {props.game.gameStage}
-          {/* {props.nextDisplay} */}
+          {props.nextDisplay}
         </Button>
       </center>
       <Grid
@@ -63,6 +63,10 @@ const mapState = (state) => {
     userId: state.auth.id,
     rests: state.rest.rests,
     game: state.game,
+    nextDisplay:
+      state.game.gameStage === state.rest.rests.length
+        ? 'End Game'
+        : 'Continue your Adventure!',
   };
 };
 const mapDispatch = {
