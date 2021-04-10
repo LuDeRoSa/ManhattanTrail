cf23d0bfa9cceb74809e0e46a1006a9726959796
Samir Jain <sjsamphex@gmail.com>
2021-04-10 10:36:24 -0700

    stronger mobile compat by removing hardcoded margin and using container instead

--- client/components/AboutUs.js
+++ client/components/AboutUs.js
@@ -14,8 +14,7 @@ import OurTeam from './OurTeam';
 const useStyles = makeStyles((theme) => ({
   root: {},
   paper: {
-    margin: '6rem',
-    padding: '2rem',
+    padding: '1rem',
     backgroundColor: theme.palette.background.default,
   },
   card: {
@@ -33,119 +32,121 @@ export const AboutUs = (props) => {
   const classes = useStyles();
 
   return (
-    <Paper variant="outlined" square className={classes.paper}>
-      <div className="tech-heading">Technologies</div>
-      <Grid
-        container
-        spacing={1}
-        direction="row"
-        alignItems="center"
-        justify="space-around"
-      >
-        <Grid item xs={5} sm={5} lg={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              component="img"
-              alt="react-logo"
-              // height="140"
-              image="./img/react-logo.png"
-            />
-            <b> React.js </b>
-          </Card>
-        </Grid>
-        <Grid item xs={5} sm={5} lg={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              component="img"
-              alt="material-ui-logo"
-              // height="140"
-              image="./img/material-ui-logo.png"
-            />
-            <b>Material UI</b>
-          </Card>
-        </Grid>
-        <Grid item xs={5} sm={5} lg={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              component="img"
-              alt="google-maps-logo"
-              // height="140"
-              image="./img/google-maps-logo.png"
-            />
-            <b>Google Maps API</b>
-          </Card>
-        </Grid>
+    <Container>
+      <Paper variant="outlined" square className={classes.paper}>
+        <div className="tech-heading">Technologies</div>
+        <Grid
+          container
+          spacing={1}
+          direction="row"
+          alignItems="center"
+          justify="space-around"
+        >
+          <Grid item xs={5} sm={5} lg={3}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                component="img"
+                alt="react-logo"
+                // height="140"
+                image="./img/react-logo.png"
+              />
+              <b> React.js </b>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                component="img"
+                alt="material-ui-logo"
+                // height="140"
+                image="./img/material-ui-logo.png"
+              />
+              <b>Material UI</b>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                component="img"
+                alt="google-maps-logo"
+                // height="140"
+                image="./img/google-maps-logo.png"
+              />
+              <b>Google Maps API</b>
+            </Card>
+          </Grid>
 
-        <Grid item xs={5} sm={5} lg={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              component="img"
-              alt="logo"
-              // height="140"
-              image="./img/node-js-logo.svg"
-            />
-            <b>Node.js</b>
-          </Card>
-        </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                component="img"
+                alt="logo"
+                // height="140"
+                image="./img/node-js-logo.svg"
+              />
+              <b>Node.js</b>
+            </Card>
+          </Grid>
 
-        <Grid item xs={5} sm={5} lg={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              component="img"
-              alt="logo"
-              // height="140"
-              image="./img/express-logo.jpeg"
-            />
-            <b>Express</b>
-          </Card>
-        </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                component="img"
+                alt="logo"
+                // height="140"
+                image="./img/express-logo.jpeg"
+              />
+              <b>Express</b>
+            </Card>
+          </Grid>
 
-        <Grid item xs={5} sm={5} lg={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              component="img"
-              alt="logo"
-              // height="140"
-              image="./img/postgresql-logo.png"
-            />
-            <b>PostgreSQL</b>
-          </Card>
-        </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                component="img"
+                alt="logo"
+                // height="140"
+                image="./img/postgresql-logo.png"
+              />
+              <b>PostgreSQL</b>
+            </Card>
+          </Grid>
 
-        <Grid item xs={5} sm={5} lg={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              component="img"
-              alt="logo"
-              // height="140"
-              image="./img/oauth-logo.png"
-            />
-            <b>OAuth 2.0</b>
-          </Card>
-        </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                component="img"
+                alt="logo"
+                // height="140"
+                image="./img/oauth-logo.png"
+              />
+              <b>OAuth 2.0</b>
+            </Card>
+          </Grid>
 
-        <Grid item xs={5} sm={5} lg={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              component="img"
-              alt="logo"
-              // height="140"
-              image="./img/sequelize-logo.png"
-            />
-            <b>Sequelize</b>
-          </Card>
+          <Grid item xs={12} sm={6} lg={3}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                component="img"
+                alt="logo"
+                // height="140"
+                image="./img/sequelize-logo.png"
+              />
+              <b>Sequelize</b>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
-      <OurTeam />
-    </Paper>
+        <OurTeam />
+      </Paper>
+    </Container>
   );
 };
 
