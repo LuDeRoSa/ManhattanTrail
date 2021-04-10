f90ce0c3f3bbd049fb49bc2a21b22af1a0787373
Samir Jain <sjsamphex@gmail.com>
2021-04-10 10:27:39 -0700

    fix imports of our team

--- client/components/OurTeam.js
+++ client/components/OurTeam.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 import makeStyles from '@material-ui/core/styles/makeStyles';
-import createStyles from '@material-ui/core/styles/createStyles';
 import './Style/AboutUs.css';
-import { StylesProvider } from '@material-ui/styles';
-import { CardMedia } from '@material-ui/core';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
